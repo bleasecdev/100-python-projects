97b63d9a9cb4f34f362b67ec8374df570548f11a
Blease Carson <bleasecarson@gmail.com>
2023-01-19 21:21:28 -0500

    adjusted approach using for loop

--- python-hangman/game.py
+++ python-hangman/game.py
@@ -5,17 +5,14 @@ word_list = ["hired", "wealthy", "magic", "love", "dance"]
 # generate a random word. 
 random_word = random.choice(word_list)
 
+
 guess = input("Guess a letter.\n")
 
-output = ""
 for char in random_word:
-    output += '_'
-print(output)
+    if guess == char:
+        print("right")
+    else:
+        print("wrong")
 
 
-
-
-
-
-if guess in random_word:
-    pass
+    
