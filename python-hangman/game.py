7224c13adf9eab250f1e3e1913b4d5e4ffd67aca
Blease Carson <bleasecarson@gmail.com>
2023-01-20 15:08:09 -0500

    hangman application is fully functioning.

--- python-hangman/game.py
+++ python-hangman/game.py
@@ -72,14 +72,6 @@ lives = 6
 def print_stages(stage):
     print(stages[stage])
 
-
-
-
-print_stages(lives)
-
-print(random_word)
-
-
 for _ in range(len(random_word)):
     display += "_"
 print(display)
@@ -87,6 +79,7 @@ print(display)
 end_of_game = False
 
 while not end_of_game:
+    print_stages(lives)
     # Ask the user to guess a letter.  
     guess = input("Guess a letter:").lower()
 #Check if the letter the user guessed is in the word
@@ -95,6 +88,13 @@ while not end_of_game:
         if letter == guess:
             display[position] = letter
     print(display)
+
+    if guess not in random_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You Lose!")
+            
     if "_" not in display:
         end_of_game = True     
         print("You win.")
