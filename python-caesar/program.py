412bd682b48e66fae50ca096cec3987ce310353a
Blease Carson <bleasecarson@gmail.com>
2023-01-21 23:45:35 -0500

    declared variables and began to write encryption function.

--- python-caesar/program.py
+++ python-caesar/program.py
@@ -1,3 +1,25 @@
 import program_art
 
 print(program_art.logo)
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l' 'm', 'n', 'o', 'p', 'q', 'r', 
+'s','t', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# Prompt user for encryption or decryption. 
+command = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+message = input("Type your message:\n")
+shift_num = input("Type shift number:\n")
+
+
+def encryption(message, shift_num):
+    output = ""
+    for char in message:
+        for letter in letters:
+            if char == letter:
+                return [char]
+
+
+print(encryption(message, shift_num))
+
+
