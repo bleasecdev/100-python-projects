17239215c8e2dfec88d2693efa18bf8a968ca571
Blease Carson <bleasecarson@gmail.com>
2023-01-23 21:02:31 -0500

    Completed fully functioning secret auction app.

--- python-secret-auction/program.py
+++ python-secret-auction/program.py
@@ -1,17 +1,28 @@
 import os
+import art
 
 
+dict = {}
 stop = False
+
+# Program Logo
+print(art.logo)
 
 # While loop that captures information for bidding while stop is equal to False.
 while stop == False:
     name = input("What is your name?:")
     bid = input("What is your bid?: $")
     condition = input("Are there any other bidders?:\n").lower()
-    # Clears operating system.
-    os.system('clear')
-    # switches while loop off. 
+
+# Add key value pairs to the dictionary
+    dict[name] = bid
+
+# switches while loop off. 
     if condition == 'no':
         stop = True
-
+        print(f"The winner of the auction is {max(dict, key = dict.get)} who bid ${dict.get(max(dict, key = dict.get))}.")
+        
+    if condition == 'yes':
+    #  Clears operating system.
+        os.system('clear')
 
