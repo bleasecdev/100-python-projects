e3d35d5727e0afe2cad5e4b2ceec92cca3f58003
Blease Carson <bleasecarson@gmail.com>
2023-01-25 04:11:55 -0500

    Added program artwork and created recursive function.

--- python-calculator/calculator.py
+++ python-calculator/calculator.py
@@ -22,7 +22,8 @@
 
 
 # calculator()
-
+import art
+print(art.logo)
 
    
 def add(n1, n2):
@@ -44,24 +45,30 @@ operations = {
     "*": mul
 }
 
-n1 = int(input("What's the first number?: "))
-for key in operations.keys():
-    print(key)
+def calculator():
+
+    n1 = int(input("What's the first number?: "))
+
     should_continue = True
 
-while should_continue:
-    operation_symbol = input("Pick an operation from the line above: ")
-    n2 = int(input("What is the next number?: "))
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(n1, n2)
+    while should_continue:
+        for key in operations.keys():
+            print(key)
+
+        operation_symbol = input("Pick an operation from the line above: ")
+        n2 = int(input("What is the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(n1, n2)
 
 
-    print(f"{n1} {operation_symbol} {n1} = {answer}")
+        print(f"{n1} {operation_symbol} {n2} = {answer}")
 
-    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
-        n1 = answer
-    else:
-        should_continue = False
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+            n1 = answer
+        else:
+            should_continue = False
+            calculator()
         
+calculator()
 
 
