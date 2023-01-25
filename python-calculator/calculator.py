3b2fe46b193a3bd50ffa75333779fb470e1ca203
Blease Carson <bleasecarson@gmail.com>
2023-01-25 04:13:23 -0500

    Update

--- python-calculator/calculator.py
+++ python-calculator/calculator.py
@@ -1,28 +1,5 @@
-
-# def calculator():
-#     output = None
-#     stop = False
-    
-#     while stop == False:
-#         number = int(input("What's the first number?: "))
-#         print(['+', '-', '*', '/'])
-#         operator = input("{{Pick an operation: ")
-#         next_number = int(input("What is the next number?: "))
-        
-        
-        
-#         cont = input(f"Type 'y' to continue calculating with {output}, or type 'n' to start a new calculation: ")
-#         if cont == 'n':
-#             print(output) 
-#             stop = True
-#         if cont == 'y':
-#             number = output
-#             stop = True
-    
-
-
-# calculator()
 import art
+
 print(art.logo)
 
    
