476a9ca734f15ff9f6256846a24f014dd44132b3
Blease Carson <bleasecarson@gmail.com>
2023-02-23 18:13:10 -0500

    Updated data file using Open Trivia API.

--- python-quiz-game/data.py
+++ python-quiz-game/data.py
@@ -1,14 +1,21 @@
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"Tony Hawk&#039;s Pro Skater was released in 1999.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"In Half-Life 2, if you play the zombies&#039; speech in reverse, they actually speak coherent English.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"TF2: The Medic will be credited for an assist if he heals a Spy that successfully saps a building.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"Mortal Kombat was almost based on Jean-Claude Van Damme movie.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"David Baszucki was a co-founder of ROBLOX Corporation.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"In &quot;Call Of Duty: World At War&quot;, the first appearance of the &quot;PPSH-41&quot; in &quot;Zombies&quot; was in the map &quot;Der Riese&quot;.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"In &quot;Call Of Duty: Zombies&quot;, you can upgrade the &quot;Apothicon Servant&quot; in the &quot;Shadows Of Evil&quot; map.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"In the Season One Championship of &quot;League of Legends&quot;, the highest achievable rank was Diamond.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"The scrapped Sonic the Hedgehog 2 level &quot;Hidden Palace Zone&quot; was later reused in the iOS port of the game. ","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"Shang Tsung is a playable character in Mortal Kombat XL.","correct_answer":"False","incorrect_answers":["True"]}
+    ]
