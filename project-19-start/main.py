732550d2cdfa9eb3396955693d3d41f863adc067
Blease Carson <bleasecarson@gmail.com>
2023-03-03 21:59:11 -0500

    initial commit for turtle race.

--- project-19-start/main.py
+++ project-19-start/main.py
@@ -3,31 +3,8 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
+screen.setup(width=500, height=400)
 
-def move_forwards():
-    tim.fd(10)
-
-def move_backwards():
-    tim.bk(10)
-
-def counter_clockwise():
-    tim.lt(10)
-
-def clockwise():
-    tim.rt(10)
-
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
-
-screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=counter_clockwise)
-screen.onkey(key="d", fun=clockwise)
-screen.onkey(clear, "c")
 screen.exitonclick()
 
 
